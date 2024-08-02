8f3b2d9c510be764e312017a89d6a4160fb3f78b
mohammad shahindust <48916069+DevTomy@users.noreply.github.com>
2024-08-02 20:41:43 +0330

    fix this shit

--- lib/src/widgets.dart
+++ lib/src/widgets.dart
@@ -53,7 +53,7 @@ class SkeletonAvatar extends StatelessWidget {
                       style.maxHeight ?? constraints.maxHeight)
                   : style.height,
               decoration: BoxDecoration(
-                color: Theme.of(context).backgroundColor,
+                color: Colors.gray,
                 shape: style.shape,
                 borderRadius:
                     style.shape != BoxShape.circle ? style.borderRadius : null,
